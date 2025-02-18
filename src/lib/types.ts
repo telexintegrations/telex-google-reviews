53aa70f2770eed8183f794dd95a55ac8c060b523
Prince Eze <pchidireeze@gmail.com>
2025-02-18 20:28:01 +0100

    feat: update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,10 +3,42 @@ export type TickRequest = {
   return_url: "string";
   settings: [
     {
-      label: "business-1";
+      label: "interval";
       type: "text";
       required: true;
-      default: "";
+      default: string;
+    },
+    {
+      label: "Business Place ID";
+      type: "text";
+      required: true;
+      default: string;
     }
   ];
 };
+
+export interface Place {
+  html_attributions: any[];
+  result: Result;
+  status: string;
+}
+
+export interface Result {
+  name: string;
+  rating: number;
+  reviews: Review[];
+  user_ratings_total: number;
+}
+
+export interface Review {
+  author_name: string;
+  author_url: string;
+  language: string;
+  original_language: string;
+  profile_photo_url: string;
+  rating: number;
+  relative_time_description: string;
+  text: string;
+  time: number; // Time since midnight, January 1, 1970 UTC.
+  translated: boolean;
+}
